d210f53929070ee320ff5fefc1808e1c93d0237a
buksuwebteam <142291347+buksuwebteam@users.noreply.github.com>
2024-08-31 01:59:42 +0800

    chore: code reformatting

--- buksu_framework/services/gns.py
+++ buksu_framework/services/gns.py
@@ -1,5 +1,6 @@
 from buksu_framework.base import BaseService
-from buksu_framework.mixins import ListMixin, RetrieveMixin, CRUDMixin
+from buksu_framework.mixins import CRUDMixin
+ 
 
 class GlobalNotificationService(BaseService, CRUDMixin):
     """
@@ -14,23 +15,21 @@ class GlobalNotificationService(BaseService, CRUDMixin):
 
     def read(self, id):
         return self.post(f"{id}/read")
-    
+
     def unread(self, id):
         return self.post(f"{id}/unread")
-    
+
     def mark_all_as_read(self):
         return self.post("mark-all-as-read")
-    
+
     def mark_all_as_unread(self):
         return self.post("mark-all-as-unread")
-    
+
     def notify(self, user_id, data):
         return self.post(f"{user_id}/notify", data=data)
-    
+
     def notify_group(self, group_id, data):
         return self.post(f"{group_id}/notify", data=data)
-    
+
     def notify_all(self, data):
         return self.post("notify-all", data=data)
-
-
